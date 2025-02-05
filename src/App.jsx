0d0b0413fe6061e94b3e8ed95e4284c8042f9394
Joaquín Cavitelli <75315685+JoaquinCaviltelli@users.noreply.github.com>
2025-02-05 14:55:43 -0300

    v1.3

--- src/App.jsx
+++ src/App.jsx
@@ -85,12 +85,15 @@ function App() {
           backgroundImage: `url(${bgImage})`,
           height: `${windowHeight}px`, 
         }}
+        loading="lazy"
       >
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex flex-col justify-between text-white w-full max-w-7xl mx-auto p-6">
           <Navbar />
           <div className="md:max-w-3xl max-w-xl">
-            
+            <h1 className="text-5xl md:text-6xl font-[800] lg:mb-6 mb-6">
+              Optimiza el transporte de tus cargas
+            </h1>
             <p className="lg:text-xl lg:mb-16 mb-6">
               Transporta el Apilador Eléctrico Autocargable junto con tu carga,
               y lleva la productividad al siguiente nivel.
